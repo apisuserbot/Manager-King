41d8a8be5c25a6db82e9fe197f878f69d39f3829
Prakasaka - the illuminator <44729591+Prakasaka@users.noreply.github.com>
2019-05-27 21:57:52 +0530

    Update ud.py

--- haruka/modules/ud.py
+++ haruka/modules/ud.py
@@ -7,12 +7,20 @@ from haruka import dispatcher
 from requests import get
 
 @run_async
-def ud(bot: Bot, update: Update):
-  message = update.effective_message
-  text = message.text[len('/ud '):]
-  results = get(f'http://api.urbandictionary.com/v0/define?term={text}').json()
-  reply_text = f'Word: {text}\nDefinition: {results["list"][0]["definition"]}'
-  message.reply_text(reply_text)
+def ud(bot: Bot, update: Update, args):
+        term = ' '.join(args)
+        ud_api = "http://api.urbandictionary.com/v0/define?term=" + term
+        ud_reply = json.loads(requests.get(ud_api).content)['list']
+        if len(args) == 0:
+            update.message.reply_text("USAGE: /ud <Word>")
+        elif len(ud_reply) != 0:
+            ud = ud_reply[0]
+            reply_text = "<b>{0}</b>\n<a href='{1}'>{1}</a>\n<i>By {2}</i>\n\nDefinition: {3}\n\nExample: {4}".format(
+                ud['word'], ud['permalink'], ud['author'], ud['definition'], ud['example'])
+            update.message.reply_text(reply_text, parse_mode='HTML')
+        else:
+            update.message.reply_text("Term not found")
+
 
 __help__ = """
  - /ud:{word} Type the word or expression you want to search use. like /ud telegram Word: Telegram Definition: A once-popular system of telecommunications, in which the sender would contact the telegram service and speak their [message] over the [phone]. The person taking the message would then send it, via a teletype machine, to a telegram office near the receiver's [address]. The message would then be hand-delivered to the addressee. From 1851 until it discontinued the service in 2006, Western Union was the best-known telegram service in the world.
