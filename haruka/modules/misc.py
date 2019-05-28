56ebcf43cec8b5e1db47272dc2d905f9adfcfa4c
Prakasaka - the illuminator <44729591+Prakasaka@users.noreply.github.com>
2019-05-28 18:53:35 +0530

    Update misc.py

--- haruka/modules/misc.py
+++ haruka/modules/misc.py
@@ -561,8 +561,7 @@ def ud(bot: Bot, update: Update, args):
             update.message.reply_text("USAGE: /ud <Word>")
         elif len(ud_reply) != 0:
             ud = ud_reply[0]
-            reply_text = "<b>{0}</b>\n<a href='{1}'>{1}</a>\n<i>By {2}</i>\n\nDefinition: {3}\n\nExample: {4}".format(
-                ud['author'], ud['definition'], ud['example'])
+            reply_text = "<b>{0}</b>\n<a href='{1}'>{1}</a>\n<i>By {2}</i>\n\nDefinition: {3}\n\nExample: {4}".format(ud['definition'], ud['example'])
             update.message.reply_text(reply_text, parse_mode='HTML')
         else:
             update.message.reply_text("Term not found")
