4ab28d615944f10772484d20ae8bdd2155842278
Prakasaka - the illuminator <44729591+Prakasaka@users.noreply.github.com>
2019-05-28 06:33:52 +0530

    Update stickers.py

--- haruka/modules/stickers.py
+++ haruka/modules/stickers.py
@@ -126,7 +126,7 @@ def makepack_internal(msg, user, png_sticker, emoji, bot):
     name = name[:50]
     packname = f"c{user.id}_by_{bot.username}"
     try:
-        success = bot.create_new_sticker_set(user.id, packname, name + "'s ThugBot",
+        success = bot.create_new_sticker_set(user.id, packname, name + "'s Stickers Pack",
                                              png_sticker=png_sticker,
                                              emojis=emoji)
     except TelegramError as e:
