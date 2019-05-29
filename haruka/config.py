30b62558349f9f877e64be25f293a2dc018d6ee0
Prakasaka - the illuminator <44729591+Prakasaka@users.noreply.github.com>
2019-05-29 22:06:25 +0530

    Update config.py

--- haruka/config.py
+++ haruka/config.py
@@ -16,7 +16,7 @@ class Config(object):
 
     # RECOMMENDED
     SQLALCHEMY_DATABASE_URI = ''  # needed for any database modules
-    MESSAGE_DUMP = -1001326118005  # needed to make sure 'save from' messages persist
+    MESSAGE_DUMP = # needed to make sure 'save from' messages persist
     LOAD = []
     NO_LOAD = ['translation', 'sed']
     WEBHOOK = ANYTHING
