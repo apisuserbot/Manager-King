96825e0c6a2d97e6a31c3c1b745a615389ef392f
Hitalo-Kun <40531911+HitaloKun@users.noreply.github.com>
2019-10-30 11:05:18 -0300

    Update __init__.py

--- haruka/__init__.py
+++ haruka/__init__.py
@@ -19,53 +19,89 @@ LOGGER.info("Starting haruka...")
 #    quit(1)
     
 ENV = bool(os.environ.get('ENV', False))
-    
+
 if ENV:
     TOKEN = os.environ.get('TOKEN', None)
-try:
-    OWNER_ID = int(Config.OWNER_ID)
-except ValueError:
-    raise Exception("Your OWNER_ID variable is not a valid integer.")
+    try:
+        OWNER_ID = int(os.environ.get('OWNER_ID', None))
+    except ValueError:
+        raise Exception("Your OWNER_ID env variable is not a valid integer.")
 
-try:
+    MESSAGE_DUMP = os.environ.get('MESSAGE_DUMP', None)
+    OWNER_USERNAME = os.environ.get("OWNER_USERNAME", None)
+
+    try:
+        SUDO_USERS = set(int(x) for x in os.environ.get("SUDO_USERS", "").split())
+    except ValueError:
+        raise Exception("Your sudo users list does not contain valid integers.")
+
+    try:
+        SUPPORT_USERS = set(int(x) for x in os.environ.get("SUPPORT_USERS", "").split())
+    except ValueError:
+        raise Exception("Your support users list does not contain valid integers.")
+
+    try:
+        WHITELIST_USERS = set(int(x) for x in os.environ.get("WHITELIST_USERS", "").split())
+    except ValueError:
+        raise Exception("Your whitelisted users list does not contain valid integers.")
+
+    WEBHOOK = bool(os.environ.get('WEBHOOK', False))
+    URL = os.environ.get('URL', "")  # Does not contain token
+    PORT = int(os.environ.get('PORT', 5000))
+    CERT_PATH = os.environ.get("CERT_PATH")
+
+    DB_URI = os.environ.get('DATABASE_URL')
+    LOAD = os.environ.get("LOAD", "").split()
+    NO_LOAD = os.environ.get("NO_LOAD", "translation").split()
+    DEL_CMDS = bool(os.environ.get('DEL_CMDS', False))
+    STRICT_ANTISPAM = bool(os.environ.get('STRICT_ANTISPAM', False))
+    WORKERS = int(os.environ.get('WORKERS', 8))
+    BAN_STICKER = os.environ.get('BAN_STICKER', 'CAADAgADEAgAAgi3GQL9YQyT_kBpQwI')
+    ALLOW_EXCL = os.environ.get('ALLOW_EXCL', False)
+    API_WEATHER = os.environ.get('API_OPENWEATHER', None)
+
+else:
+    from haruka.config import Development as Config
+    TOKEN = Config.API_KEY
+    try:
+        OWNER_ID = int(Config.OWNER_ID)
+    except ValueError:
+        raise Exception("Your OWNER_ID variable is not a valid integer.")
+
     MESSAGE_DUMP = Config.MESSAGE_DUMP
-except ValueError:
-    raise Exception("Your MESSAGE_DUMP must be set.")
+    OWNER_USERNAME = Config.OWNER_USERNAME
 
-#MESSAGE_DUMP = Config.MESSAGE_DUMP
-OWNER_USERNAME = Config.OWNER_USERNAME
+    try:
+        SUDO_USERS = set(int(x) for x in Config.SUDO_USERS or [])
+    except ValueError:
+        raise Exception("Your sudo users list does not contain valid integers.")
 
-try:
-    SUDO_USERS = set(int(x) for x in Config.SUDO_USERS or [])
-except ValueError:
-    raise Exception("Your sudo users list does not contain valid integers.")
+    try:
+        SUPPORT_USERS = set(int(x) for x in Config.SUPPORT_USERS or [])
+    except ValueError:
+        raise Exception("Your support users list does not contain valid integers.")
 
-try:
-    SUPPORT_USERS = set(int(x) for x in Config.SUPPORT_USERS or [])
-except ValueError:
-    raise Exception("Your support users list does not contain valid integers.")
+    try:
+        WHITELIST_USERS = set(int(x) for x in Config.WHITELIST_USERS or [])
+    except ValueError:
+        raise Exception("Your whitelisted users list does not contain valid integers.")
 
-try:
-    WHITELIST_USERS = set(int(x) for x in Config.WHITELIST_USERS or [])
-except ValueError:
-    raise Exception("Your whitelisted users list does not contain valid integers.")
+    WEBHOOK = Config.WEBHOOK
+    URL = Config.URL
+    PORT = Config.PORT
+    CERT_PATH = Config.CERT_PATH
 
-WEBHOOK = Config.WEBHOOK
-URL = Config.URL
-PORT = Config.PORT
-CERT_PATH = Config.CERT_PATH
+    DB_URI = Config.SQLALCHEMY_DATABASE_URI
+    DONATION_LINK = Config.DONATION_LINK
+    LOAD = Config.LOAD
+    NO_LOAD = Config.NO_LOAD
+    DEL_CMDS = Config.DEL_CMDS
+    STRICT_ANTISPAM = Config.STRICT_ANTISPAM
+    WORKERS = Config.WORKERS
+    BAN_STICKER = Config.BAN_STICKER
+    ALLOW_EXCL = Config.ALLOW_EXCL
+    API_WEATHER = Config.API_OPENWEATHER
 
-DB_URI = Config.SQLALCHEMY_DATABASE_URI
-LOAD = Config.LOAD
-NO_LOAD = Config.NO_LOAD
-DEL_CMDS = Config.DEL_CMDS
-STRICT_ANTISPAM = Config.STRICT_ANTISPAM
-WORKERS = Config.WORKERS
-BAN_STICKER = Config.BAN_STICKER
-ALLOW_EXCL = Config.ALLOW_EXCL
-MAPS_API = Config.MAPS_API
-API_WEATHER = Config.API_OPENWEATHER
-DEEPFRY_TOKEN = os.environ.get('DEEPFRY_TOKEN', "")
 
 SUDO_USERS.add(OWNER_ID)
 
