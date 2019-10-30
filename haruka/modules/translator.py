b45d6b205563d2d0aa95e2e50047a6c7da3df9a3
Hitalo-Kun <40531911+HitaloKun@users.noreply.github.com>
2019-10-30 08:36:22 -0300

    Add files via upload

--- haruka/modules/translator.py
+++ haruka/modules/translator.py
@@ -12,17 +12,19 @@ from googletrans import Translator
 
 @run_async
 def do_translate(bot: Bot, update: Update, args: List[str]):
-    short_name = "Created By @MidukkiBot 😬"
     msg = update.effective_message # type: Optional[Message]
     lan = " ".join(args)
-    to_translate_text = msg.reply_to_message.text
+    try:
+        to_translate_text = msg.reply_to_message.text
+    except:
+        return
     translator = Translator()
     try:
         translated = translator.translate(to_translate_text, dest=lan)
         src_lang = translated.src
         translated_text = translated.text
         msg.reply_text("Translated from {} to {}.\n {}".format(src_lang, lan, translated_text))
-    except:
+    except :
         msg.reply_text("Error")
 
 
