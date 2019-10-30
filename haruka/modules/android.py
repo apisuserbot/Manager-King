4b4b60ad6ba5c5c587e0ea0e0c298b07ec064054
Hitalo-Kun <40531911+HitaloKun@users.noreply.github.com>
2019-10-30 19:12:41 -0300

    Adding the commands /TWRP /Magisk /Device

--- haruka/modules/android.py
+++ haruka/modules/android.py
@@ -27,6 +27,81 @@ from requests import get
 
 LOGGER.info("Original Android Modules by @RealAkito on Telegram")
 
+GITHUB = 'https://github.com'
+DEVICES_DATA = 'https://raw.githubusercontent.com/androidtrackers/certified-android-devices/master/devices.json'
+
+@run_async
+def magisk(bot, update):
+    url = 'https://raw.githubusercontent.com/topjohnwu/magisk_files/'
+    releases = ""
+    for type, path  in {"Stable":"master/stable", "Beta":"master/beta", "Canary":"canary/release"}.items():
+        data = get(url + path + '.json').json()
+        releases += f'{type}: [ZIP v{data["magisk"]["version"]}]({data["magisk"]["link"]}) | ' \
+                    f'[APP v{data["app"]["version"]}]({data["app"]["link"]}) | ' \
+                    f'[Uninstaller]({data["uninstaller"]["link"]})\n'
+                        
+
+    update.message.reply_text("*Latest Magisk Releases:*\n{}".format(releases),
+                               parse_mode=ParseMode.MARKDOWN, disable_web_page_preview=True)
+
+@run_async
+def device(bot, update, args):
+    if len(args) == 0:
+        update.effective_message.reply_text("No codename provided, write a codename for fetching informations.")
+        return
+    device = " ".join(args)
+    found = [
+        i for i in get(DEVICES_DATA).json()
+        if i["device"] == device or i["model"] == device
+    ]
+    if found:
+        reply = f'Search results for {device}:\n\n'
+        for item in found:
+            brand = item['brand']
+            name = item['name']
+            codename = item['device']
+            model = item['model']
+            reply += f'<b>{brand} {name}</b>\n' \
+                f'Model: <code>{model}</code>\n' \
+                f'Codename: <code>{codename}</code>\n\n'                
+    else:
+        reply = f"Couldn't find info about {device}!\n"
+    update.message.reply_text("{}".format(reply),
+                               parse_mode=ParseMode.HTML, disable_web_page_preview=True)
+        
+        
+def twrp(bot, update, args):
+    if len(args) == 0:
+        update.effective_message.reply_text("No codename provided, write a codename for fetching informations.")
+        return
+    device = " ".join(args)
+    url = get(f'https://eu.dl.twrp.me/{device}/')
+    if url.status_code == 404:
+        reply = f"Couldn't find twrp downloads for {device}!\n"
+        return
+    reply = f'*Latest Official TWRP for {device}*\n'            
+    db = get(DEVICES_DATA).json()
+    newdevice = device.strip('lte') if device.startswith('beyond') else device
+    for dev in db:
+        if (dev['device'] == newdevice) or (dev['model'] == newdevice):
+            brand = dev['brand']
+            name = dev['name']
+            reply += f'*{brand} - {name}*\n'
+            break
+    page = BeautifulSoup(url.content, 'lxml')
+    date = page.find("em").text.strip()
+    reply += f'*Updated:* {date}\n'
+    trs = page.find('table').find_all('tr')
+    row = 2 if trs[0].find('a').text.endswith('tar') else 1
+    for i in range(row):
+        download = trs[i].find('a')
+        dl_link = f"https://eu.dl.twrp.me{download['href']}"
+        dl_file = download.text
+        size = trs[i].find("span", {"class": "filesize"}).text
+        reply += f'[{dl_file}]({dl_link}) - {size}\n'
+
+    update.message.reply_text("{}".format(reply),
+                               parse_mode=ParseMode.MARKDOWN, disable_web_page_preview=True)
 
 @run_async
 def havoc(bot: Bot, update: Update):
@@ -508,6 +583,11 @@ def bootleggers(bot: Bot, update: Update):
 
 __help__ = """
 *This module is made with love by* @RealAkito *and code beauty by* @kandnub
+*Useful tools*
+ - /magisk: gets the latest magisk release for Stable/Beta/Canary
+ - /device <codename>: gets android device basic info from its codename
+ - /twrp <codename>: gets latest twrp for the android device using the codename
+
  *Device Specific Rom*
  - /pearl <device>: Get the Pearl Rom
  - /havoc <device>: Get the Havoc Rom
@@ -519,6 +599,7 @@ __help__ = """
  - /pixys <device>: Get the Pixy Rom
  - /los <device>: Get the LineageOS Rom
  - /bootleggers <device>: Get the Bootleggers Rom
+ 
  *GSI*
  - /phh: Get the lastest Phh AOSP Oreo GSI!
  - /descendant: Get the lastest Descendant GSI!
@@ -527,7 +608,9 @@ __help__ = """
 
 __mod_name__ = "Android"
 
-
+MAGISK_HANDLER = DisableAbleCommandHandler("magisk", magisk)
+DEVICE_HANDLER = DisableAbleCommandHandler("device", device, pass_args=True)
+TWRP_HANDLER = DisableAbleCommandHandler("twrp", twrp, pass_args=True)
 GETAEX_HANDLER = DisableAbleCommandHandler("aex", getaex, pass_args=True, admin_ok=True)
 MIUI_HANDLER = DisableAbleCommandHandler("miui", miui, admin_ok=True)
 EVO_HANDLER = DisableAbleCommandHandler("evo", evo, admin_ok=True)
@@ -543,6 +626,9 @@ PIXYS_HANDLER = DisableAbleCommandHandler("pixys", pixys, admin_ok=True)
 LOS_HANDLER = DisableAbleCommandHandler("los", los, admin_ok=True)
 BOOTLEGGERS_HANDLER = DisableAbleCommandHandler("bootleggers", bootleggers, admin_ok=True)
 
+dispatcher.add_handler(MAGISK_HANDLER)
+dispatcher.add_handler(DEVICE_HANDLER)
+dispatcher.add_handler(TWRP_HANDLER)
 dispatcher.add_handler(GETAEX_HANDLER)
 dispatcher.add_handler(MIUI_HANDLER)
 dispatcher.add_handler(EVO_HANDLER)
