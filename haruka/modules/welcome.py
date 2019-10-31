39565918013c2e51c83cf3b5c9b3dd44b1b45ac1
Hitalo-Kun <40531911+HitaloKun@users.noreply.github.com>
2019-10-31 20:31:58 -0300

    Trying to implement pyCombotCAS_API

--- haruka/modules/welcome.py
+++ haruka/modules/welcome.py
@@ -3,6 +3,8 @@ import time
 import re
 from typing import Optional, List
 
+import haruka.modules.helper_funcs.cas_api as cas
+
 from telegram import Message, Chat, Update, Bot, User, CallbackQuery
 from telegram import ParseMode, InlineKeyboardMarkup, InlineKeyboardButton
 from telegram.error import BadRequest
@@ -18,7 +20,6 @@ from haruka.modules.helper_funcs.msg_types import get_welcome_type
 from haruka.modules.helper_funcs.string_handling import markdown_parser, \
     escape_invalid_curly_brackets, extract_time, markdown_to_html
 from haruka.modules.log_channel import loggable
-
 
 VALID_WELCOME_FORMATTERS = ['first', 'last', 'fullname', 'username', 'id', 'count', 'chatname', 'mention']
 
@@ -94,6 +95,29 @@ def send(update, message, keyboard, backup_message):
 @run_async
 def new_member(bot: Bot, update: Update):
     chat = update.effective_chat  # type: Optional[Chat]
+    user = update.effective_user  # type: Optional[User]
+    msg = update.effective_message # type: Optional[Message]
+    chat_name = chat.title or chat.first or chat.username # type: Optional:[chat name]
+    should_welc, cust_welcome, welc_type = sql.get_welc_pref(chat.id)
+    welc_mutes = sql.welcome_mutes(chat.id)
+    casPrefs = sql.get_cas_status(str(chat.id)) #check if enabled, obviously
+    autoban = sql.get_cas_autoban(str(chat.id))
+    if casPrefs and not autoban and cas.banchecker(user.id):
+        bot.restrict_chat_member(chat.id, user.id, 
+                                         can_send_messages=False, 
+                                         can_send_media_messages=False, 
+                                         can_send_other_messages=False, 
+                                         can_add_web_page_previews=False)
+        msg.reply_text("Warning! This user is CAS Banned. I have muted them to avoid spam. Ban is adviced.")
+        isUserGbanned = gbansql.is_user_gbanned(user.id)
+        report = "CAS Banned user detected: <code>{}</code>\nGlobally Banned: {}".format(user.id, isUserGbanned)
+        send_to_list(bot, SUDO_USERS + SUPPORT_USERS, report, html=True)
+    elif casPrefs and autoban and cas.banchecker(user.id):
+        chat.kick_member(user.id)
+        msg.reply_text("CAS banned user detected! User has been automatically banned!")
+        isUserGbanned = gbansql.is_user_gbanned(user.id)
+        report = "CAS Banned user detected: <code>{}</code>\nGlobally Banned: {}".format(user.id, isUserGbanned)
+        send_to_list(bot, SUDO_USERS + SUPPORT_USERS, report, html=True)
 
     should_welc, cust_welcome, cust_content, welc_type = sql.get_welc_pref(chat.id)
     cust_welcome = markdown_to_html(cust_welcome)
@@ -759,11 +783,135 @@ def __chat_settings__(bot, update, chat, chatP, user):
     return "This chat has it's welcome preference set to `{}`.\n" \
            "It's goodbye preference is `{}`.".format(welcome_pref, goodbye_pref)
 
+@run_async
+@user_admin
+def setcas(bot: Bot, update: Update):
+    chat = update.effective_chat
+    msg = update.effective_message
+    split_msg = msg.text.split(' ')
+    if len(split_msg)!= 2:
+        msg.reply_text("Invalid arguments!")
+        return
+    param = split_msg[1]
+    if param == "on" or param == "true":
+        sql.set_cas_status(chat.id, True)
+        msg.reply_text("Successfully updated configuration.")
+        return
+    elif param == "off" or param == "false":
+        sql.set_cas_status(chat.id, False)
+        msg.reply_text("Successfully updated configuration.")
+        return
+    else:
+        msg.reply_text("Invalid status to set!") #on or off ffs
+        return
+
+@run_async
+@user_admin
+def setban(bot: Bot, update: Update):
+    chat = update.effective_chat
+    msg = update.effective_message
+    split_msg = msg.text.split(' ')
+    if len(split_msg)!= 2:
+        msg.reply_text("Invalid arguments!")
+        return
+    param = split_msg[1]
+    if param == "on" or param == "true":
+        sql.set_cas_autoban(chat.id, True)
+        msg.reply_text("Successfully updated configuration.")
+        return
+    elif param == "off" or param == "false":
+        sql.set_cas_autoban(chat.id, False)
+        msg.reply_text("Successfully updated configuration.")
+        return
+    else:
+        msg.reply_text("Invalid autoban definition to set!") #on or off ffs
+        return
+
+@run_async
+@user_admin
+def get_current_setting(bot: Bot, update: Update):
+    chat = update.effective_chat
+    msg = update.effective_message
+    stats = sql.get_cas_status(chat.id)
+    autoban = sql.get_cas_autoban(chat.id)
+    rtext = "<b>CAS Preferences</b>\n\nCAS Checking: {}\nAutoban: {}".format(stats, autoban)
+    msg.reply_text(rtext, parse_mode=ParseMode.HTML)
+    return
+
+@run_async
+def get_version(bot: Bot, update: Update):
+    msg = update.effective_message
+    ver = cas.vercheck()
+    msg.reply_text("CAS API version: "+ver)
+    return
+
+@run_async
+def caschecker(bot: Bot, update: Update, args: List[str]):
+    #/info logic
+    msg = update.effective_message  # type: Optional[Message]
+    user_id = extract_user(update.effective_message, args)
+    if user_id and int(user_id) != 777000:
+        user = bot.get_chat(user_id)
+    elif user_id and int(user_id) == 777000:
+        msg.reply_text("This is Telegram. Unless you manually entered this reserved account's ID, it is likely a broadcast from a linked channel.")
+        return
+    elif not msg.reply_to_message and not args:
+        user = msg.from_user
+    elif not msg.reply_to_message and (not args or (
+            len(args) >= 1 and not args[0].startswith("@") and not args[0].isdigit() and not msg.parse_entities(
+        [MessageEntity.TEXT_MENTION]))):
+        msg.reply_text("I can't extract a user from this.")
+        return
+    else:
+        return
+
+    text = "<b>CAS Check</b>:" \
+           "\nID: <code>{}</code>" \
+           "\nFirst Name: {}".format(user.id, html.escape(user.first_name))
+    if user.last_name:
+        text += "\nLast Name: {}".format(html.escape(user.last_name))
+    if user.username:
+        text += "\nUsername: @{}".format(html.escape(user.username))
+    text += "\n\nCAS Banned: "
+    result = cas.banchecker(user.id)
+    text += str(result)
+    if result:
+        parsing = cas.offenses(user.id)
+        if parsing:
+            text += "\nTotal of Offenses: "
+            text += str(parsing)
+        parsing = cas.timeadded(user.id)
+        if parsing:
+            text += "\nAdded: "
+            text += str(parsing)
+            text += "\n\nAll times are in UTC"
+    update.effective_message.reply_text(text, parse_mode=ParseMode.HTML)
+
+#this sends direct request to combot server. Will return true if user is banned, false if
+#id invalid or user not banned
+@run_async
+def casquery(bot: Bot, update: Update, args: List[str]):
+    msg = update.effective_message  # type: Optional[Message]
+    try:
+        user_id = msg.text.split(' ')[1]
+    except:
+        msg.reply_text("There was a problem parsing the query.")
+        return
+    text = "Your query returned: "
+    result = cas.banchecker(user_id)
+    text += str(result)
+    msg.reply_text(text)        
 
 __help__ = """
 Give your members a warm welcome with the greetings module! Or a sad goodbye... Depends!
 
-Available commands are:
+Thanks to @nunopenim for providing his proprietary Combot Anti Spam System API - [(pyCombotCAS_API)](https://github.com/nunopenim/pyCombotCAS_API)
+
+CAS Commands:
+ - /casver: Returns the API version that the bot is currently running
+ - /cascheck: Checks you or another user for CAS BAN
+
+*Admin Only:*
  - /welcome <on/off/yes/no>: enables/disables welcome messages. If no option is given, returns the current welcome message and welcome settings. 
  - /goodbye <on/off/yes/no>: enables/disables goodbye messages. If no option is given, returns  the current goodbye message and goodbye settings.
  - /setwelcome <message>: sets your new welcome message! Markdown and buttons are supported, as well as fillings.
@@ -777,6 +925,9 @@ Available commands are:
  Note: if you want to reset the mute time to be forever, use /welcomemutetime 0m. 0 == eternal!
  - /setmutetext <new text>: Customise the "click here to prove you're human" button obtained from enabling welcomemutes.
  - /resetmutetext: resets the mute button to the default text.
+ - /setcas <on/off/true/false>: Enables/disables CAS Checking on welcome
+ - /getcas: Gets the current CAS settings
+ - /setban <on/off/true/false>: Enables/disables autoban on CAS banned user detected.
 
 Read /markdownhelp to learn about formatting your text and mentioning new users when the join!
 
@@ -820,6 +971,12 @@ SECURITY_MUTE_HANDLER = CommandHandler("welcomemutetime", security_mute, pass_ar
 SECURITY_BUTTONTXT_HANDLER = CommandHandler("setmutetext", security_text, pass_args=True, filters=Filters.group)
 SECURITY_BUTTONRESET_HANDLER = CommandHandler("resetmutetext", security_text_reset, filters=Filters.group)
 CLEAN_SERVICE_HANDLER = CommandHandler("cleanservice", cleanservice, pass_args=True, filters=Filters.group)
+SETCAS_HANDLER = CommandHandler("setcas", setcas, filters=Filters.group)
+GETCAS_HANDLER = CommandHandler("getcas", get_current_setting, filters=Filters.group)
+GETVER_HANDLER = DisableAbleCommandHandler("casver", get_version)
+CASCHECK_HANDLER = CommandHandler("cascheck", caschecker, pass_args=True)
+CASQUERY_HANDLER = CommandHandler("casquery", casquery, pass_args=True ,filters=CustomFilters.sudo_filter)
+SETBAN_HANDLER = CommandHandler("setban", setban, filters=Filters.group)
 
 help_callback_handler = CallbackQueryHandler(check_bot_button, pattern=r"check_bot_")
 
@@ -837,5 +994,11 @@ dispatcher.add_handler(SECURITY_MUTE_HANDLER)
 dispatcher.add_handler(SECURITY_BUTTONTXT_HANDLER)
 dispatcher.add_handler(SECURITY_BUTTONRESET_HANDLER)
 dispatcher.add_handler(CLEAN_SERVICE_HANDLER)
+dispatcher.add_handler(SETCAS_HANDLER)
+dispatcher.add_handler(GETCAS_HANDLER)
+dispatcher.add_handler(GETVER_HANDLER)
+dispatcher.add_handler(CASCHECK_HANDLER)
+dispatcher.add_handler(CASQUERY_HANDLER)
+dispatcher.add_handler(SETBAN_HANDLER)
 
 dispatcher.add_handler(help_callback_handler)
