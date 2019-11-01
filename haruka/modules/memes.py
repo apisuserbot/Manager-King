d8c0f77a2aa9add937149961b31988450ad551af
unknown <hitalomarquete331@gmail.com>
2019-11-01 15:27:29 -0300

    Fix 2: New Memes

--- haruka/modules/memes.py
+++ haruka/modules/memes.py
@@ -16,7 +16,7 @@ nltk.download('averaged_perceptron_tagger')
 from typing import Optional, List
 from telegram import Message, Update, Bot, User
 from telegram import MessageEntity
-from telegram.ext import Filters, MessageHandler, run_async
+from telegram.ext import Filters, MessageHandler, run_async, CommandHandler, RegexHandler
 
 from haruka import DEEPFRY_TOKEN
 from haruka import dispatcher
