eb55cb0d3a7210926bbaa341c320dddfacd80b8d
HitaloKun - イタチ <40531911+HitaloKun@users.noreply.github.com>
2019-11-28 09:24:06 -0300

    Update devs.py

--- haruka/modules/devs.py
+++ haruka/modules/devs.py
@@ -7,7 +7,7 @@ from telegram.ext import CommandHandler, run_async, Filters
 from telegram.utils.helpers import escape_markdown, mention_html
 
 from haruka import dispatcher, OWNER_ID
-from emilia.__main__ import STATS, USER_INFO
+from haruka.__main__ import STATS, USER_INFO
 from haruka.modules.disable import DisableAbleCommandHandler, DisableAbleRegexHandler
 from haruka.modules.helper_funcs.extraction import extract_user
 from haruka.modules.helper_funcs.filters import CustomFilters
