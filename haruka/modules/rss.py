3e20c60cdaf3f6db1c16ebf9b21e06873b972293
unknown <hitalomarquete331@gmail.com>
2019-12-09 16:43:33 -0300

    rss: fixed SyntaxError

--- haruka/modules/rss.py
+++ haruka/modules/rss.py
@@ -37,7 +37,7 @@ def show_url(bot, update, args):
 
                 entry_message = update.effective_message, "\n\n<b>Entry Title::</b> \n{}" \
                                 "\n\n<b>Entry Description:</b> \n{}" \
-                                "\n\n<b>Entry Link:</b> \n{}").format(html.escape(entry_title),
+                                "\n\n<b>Entry Link:</b> \n{}".format(html.escape(entry_title),
                                                                      entry_description,
                                                                      html.escape(entry_link))
                 final_message = feed_message + entry_message
