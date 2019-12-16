fe417553f761e49babf9d4b5a7fe61dd7f0ad2b3
HitaloKun - イタチ <40531911+HitaloKun@users.noreply.github.com>
2019-12-16 13:13:59 -0300

    Update PortugueseBr.py

--- haruka/modules/translations/PortugueseBr.py
+++ haruka/modules/translations/PortugueseBr.py
@@ -116,19 +116,6 @@ INSULTS_STRINGS = (
 )
 
 SLAP_TEMPLATES = (
-    "{user1} {hits} {user2} with a {item}.",
-    "{user1} {hits} {user2} in the face with a {item}.",
-    "{user1} {hits} {user2} around a bit with a {item}.",
-    "{user1} {throws} a {item} at {user2}.",
-    "{user1} grabs a {item} and {throws} it at {user2}'s face.",
-    "{user1} launches a {item} in {user2}'s general direction.",
-    "{user1} starts slapping {user2} silly with a {item}.",
-    "{user1} pins {user2} down and repeatedly {hits} them with a {item}.",
-    "{user1} grabs up a {item} and {hits} {user2} with it.",
-    "{user1} ties {user2} to a chair and {throws} a {item} at them.",
-    "{user1} gave a friendly push to help {user2} learn to swim in lava."
-
-SLAP_TEMPLATES = (
     "{user1} {hits} {user2} com {item}.",
     "{user1} {hits} {user2} na cara com {item}.",
     "{user1} {hits} {user2} no peito com {item}.",
