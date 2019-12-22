62e27b4af8dbbf2381d11358d8ae921c6b29c41f
* <hitalomarquete331@gmail.com>
2019-12-22 16:18:29 -0300

    Update blstickers.py

--- haruka/modules/blstickers.py
+++ haruka/modules/blstickers.py
@@ -356,11 +356,11 @@ def __migrate__(old_chat_id, new_chat_id):
 
 def __chat_settings__(chat_id, user_id):
 	blacklisted = sql.num_stickers_chat_filters(chat_id)
-	return (tld(user_id, "Ada `{}` daftar hitam stiker.").format(blacklisted))
+	return "There are {} blacklisted stickers".format(blacklisted)
 
 def __stats__():
-    return "{} blacklist triggers, across {} chats.".format(sql.num_blacklist_filters(),
-                                                            sql.num_blacklist_filter_chats())
+    return "{} blacklist stickers triggers, across {} chats.".format(sql.num_stickers_filters(),
+															sql.num_stickers_filter_chats())
 
 __help__ = "blstickers_help"
 
