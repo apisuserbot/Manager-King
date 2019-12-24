7719f675bf18cbedd828c1f8f3e2317ede88ad40
* <hitalomarquete331@gmail.com>
2019-12-24 18:20:06 -0300

    Update ofox.py

--- haruka/modules/ofox.py
+++ haruka/modules/ofox.py
@@ -6,6 +6,7 @@ import re
 import subprocess
 import time
 
+from haruka.modules.disable import DisableAbleCommandHandler
 from typing import Optional, List
 from telegram import Message, Chat, Update, Bot, User
 from telegram.ext.dispatcher import run_async
