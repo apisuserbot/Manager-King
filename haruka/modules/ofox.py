753d8f541c179767b7bd8117d63f56ca5145076a
* <hitalomarquete331@gmail.com>
2019-12-24 17:54:53 -0300

    Update ofox.py

--- haruka/modules/ofox.py
+++ haruka/modules/ofox.py
@@ -26,7 +26,7 @@ FOX_BETA_CHANNEL = -1001429093106
 @run_async
 def get_build_info(bot: Bot, update: Update, args: List[str]) -> str:
     codename = message.text.split('@LordHitsuki_BOT')[0][1:].lower()
-        return
+
     chat_id = message.chat.id
     chat_type = 'stable'
     files_dir = 'OrangeFox-Stable/'
