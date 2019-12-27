300a0da5386c0fac17d9c0d1151be9ffc96eab59
* <hitalomarquete331@gmail.com>
2019-12-27 16:52:47 -0300

    Fixed antiflood module

--- haruka/modules/sql/antiflood_sql.py
+++ haruka/modules/sql/antiflood_sql.py
@@ -2,7 +2,7 @@ import threading
 
 from sqlalchemy import String, Column, Integer, UnicodeText
 
-from emilia.modules.sql import SESSION, BASE
+from haruka.modules.sql import SESSION, BASE
 
 DEF_COUNT = 0
 DEF_LIMIT = 0
