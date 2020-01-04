38151f9fe500e129ae234a1544716ea03eee1fd5
Nuno Penim <nuno.penim@gmail.com>
2020-01-04 19:58:59 +0000

    github: fixed missing import

--- haruka/modules/github.py
+++ haruka/modules/github.py
@@ -9,6 +9,7 @@ import haruka.modules.helper_funcs.git_api as api
 
 from haruka import dispatcher, OWNER_ID, LOGGER, SUDO_USERS, SUPPORT_USERS
 from haruka.modules.helper_funcs.filters import CustomFilters
+from haruka.modules.helper_funcs.chat_status import user_admin
 from haruka.modules.translations.strings import tld
 
 from telegram.ext import CommandHandler, run_async, Filters
