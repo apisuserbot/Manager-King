5313bc8afb6bd9883accecedc5d9aa5190a64f27
Nuno Penim <nuno.penim@gmail.com>
2020-01-04 20:08:13 +0000

    github_sql: fixed imports

--- haruka/modules/sql/github_sql.py
+++ haruka/modules/sql/github_sql.py
@@ -2,8 +2,8 @@ import threading
 
 from sqlalchemy import Column, String, UnicodeText, func, distinct
 
-from tg_bot.modules.helper_funcs.msg_types import Types
-from tg_bot.modules.sql import SESSION, BASE
+from haruka.modules.helper_funcs.msg_types import Types
+from haruka.modules.sql import SESSION, BASE
 
 class GitHub(BASE):
     __tablename__ = "github"
