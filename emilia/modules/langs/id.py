f2d284cb063685b3abd98f57a009994c581c7dda
wherearetheavocados <49307071+bagus02@users.noreply.github.com>
2020-03-25 19:12:35 +0700

    Update id.py

--- emilia/modules/langs/id.py
+++ emilia/modules/langs/id.py
@@ -81,8 +81,8 @@ Ketika ditandai sebagai AFK, sebutan apa pun akan dibalas dengan pesan untuk men
 """,
 	"backups_help": """
 *Hanya admin:*
- - /import: balas ke file cadangan grup butler/marie/rose/emilia untuk mengimpor sebanyak mungkin, membuat transfer menjadi sangat mudah! \
- Catatan bahwa file/foto tidak dapat diimpor karena pembatasan telegram. Kecuali backup dari Emilia.
+ - /import: balas ke file cadangan grup butler/marie/rose/emilia/dirty untuk mengimpor sebanyak mungkin, membuat transfer menjadi sangat mudah! \
+ Catatan bahwa file/foto tidak dapat diimpor karena pembatasan telegram. Kecuali backup dari Dirty.
  - /export: export data grup, hanya bisa di lakukan 12 jam sekali.
 """,
 	"bans_help": """
