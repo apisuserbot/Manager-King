858237b0720117835e01283715e0c82ee64cb1f9
bagus02 <avocados@linuxmail.org>
2020-03-30 14:50:32 +0700

    Remove Commands
    Signed-off-by: bagus02 <avocados@linuxmail.org>

--- emilia/modules/dogcat.py
+++ emilia/modules/dogcat.py
@@ -119,19 +119,7 @@ def catgif(update, context):
     update.effective_message.reply_video(cat[0]["url"])
 
 
-__help__ = """
-*A module for dog and cat lovers!*
-
-*Cat commands:*
- - /dog: Get pictures of cute dogs
- - /doghd: Get images of cute dogs in high definition
- - /doggif: Get gifs of cute dogs
-
-*Dog commands:*
- - /cat: Get pictures of cute kittens
- - /cathd: Get images of cute kittens in high definition
- - /catgif: Get gifs of cute kittens
-"""
+__help__ = "dogcat_help"
 
 __mod_name__ = "Dogs and Cats"
 
