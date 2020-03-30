bc673c21f6dc56e959a0dfb91ab1066ebd09144f
bagus02 <avocados@linuxmail.org>
2020-03-30 23:12:10 +0700

    [Update time]
    Signed-off-by: bagus02 <avocados@linuxmail.org>

--- emilia/__init__.py
+++ emilia/__init__.py
@@ -84,6 +84,7 @@ if ENV:
 	CAT_API_KEY = os.environ.get('CAT_API_KEY', None)
 	DOG_API_KEY = os.environ.get('DOG_API_KEY', None)
 	API_WEATHER = os.environ.get('API_OPENWEATHER', None)
+	TIME_API_KEY = os.environ.get('TIME_API_KEY', None)
 
 else:
 	from emilia.config import Development as Config
