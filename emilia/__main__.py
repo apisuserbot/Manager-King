89ff92572c3f691b1f097899f6963549b1d4957b
apisuserbot <80087526+apisuserbot@users.noreply.github.com>
2021-08-21 14:13:17 +0700

    fixes

--- emilia/__main__.py
+++ emilia/__main__.py
@@ -154,7 +154,9 @@ def start(update, context):
                 [InlineKeyboardButton(text="⚙️ Koneksi", callback_data="main_connect")],
                 [InlineKeyboardButton(text="🏳‍🌈 Bahasa", callback_data="main_setlang"), InlineKeyboardButton(text="❓ Bantuan", url=f"https://t.me/{BOT}?start=help")]])
             update.effective_message.reply_photo(
-                MANAGER_IMG, PM_START_TEXT).format(escape_markdown(first_name), escape_markdown(context.bot.first_name), OWNER_ID),
+                MANAGER_IMG, 
+                PM_START_TEXT.(format(escape_markdown(first_name), escape_markdown(context.bot.first_name), OWNER_ID,
+                ),
                 disable_web_page_preview=True,
                 parse_mode=ParseMode.MARKDOWN,
                 reply_markup=buttons)
